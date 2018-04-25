1af559e5eab13440513d7d08466771b53d7311d1
Tommy Chan <tommytcchan@gmail.com>
2018-04-24 18:30:41 -0700

    update the sample

--- app/src/main/java/adhoclabs/co/burnersamplesdk/SampleActivity.java
+++ app/src/main/java/adhoclabs/co/burnersamplesdk/SampleActivity.java
@@ -6,9 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.Date;
+import android.widget.Toast;
 
 import co.adhoclabs.burnersdk.BurnerSDK;
 
@@ -22,6 +22,8 @@ public class SampleActivity extends AppCompatActivity {
   private TextView field1;
   private TextView field2;
 
+  private Button removeCredentialsButton;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -32,7 +34,8 @@ public class SampleActivity extends AppCompatActivity {
     setSupportActionBar(toolbar);
 
     field1 = findViewById(R.id.phone_number);
-    field2 = findViewById(R.id.phone_number_expires_at);
+    field2 = findViewById(R.id.sdk_version);
+    removeCredentialsButton = findViewById(R.id.remove_credentials);
 
     FloatingActionButton fab = findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +44,24 @@ public class SampleActivity extends AppCompatActivity {
         burnerSDK.start(SampleActivity.this);
       }
     });
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    field2.setText("Burner SDK Version: " + burnerSDK.getSdkVersion());
+
+    if (burnerSDK.getToken(this) != null) {
+      removeCredentialsButton.setVisibility(View.VISIBLE);
+      removeCredentialsButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          field1.setText(R.string.no_number_selected);
+          burnerSDK.removeCredentials(SampleActivity.this);
+          Toast.makeText(SampleActivity.this, "Credentials Removed, now login again.", Toast.LENGTH_SHORT).show();
+        }
+      });
+    }
   }
 
   @Override
@@ -53,11 +74,6 @@ public class SampleActivity extends AppCompatActivity {
         String phoneNumber = bundle.getString(BurnerSDK.IntentParams.PHONE_NUMBER);
         if (phoneNumber != null) {
           field1.setText(getString(R.string.phone_is, phoneNumber));
-        }
-
-        long phoneExpiry = bundle.getLong(BurnerSDK.IntentParams.PHONE_NUMBER_EXPIRES_AT);
-        if (phoneExpiry != 0L) {
-          field2.setText(getString(R.string.phone_expires_at, new Date(1000 * phoneExpiry)));
         }
 
       } else { // oauth failed, get reason
